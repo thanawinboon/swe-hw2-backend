c2d59286e6e8833adc4962c6b852181062989fa0
mythicalbadger <jussy12.work@gmail.com>
2024-01-12 10:50:08 +0700

    feat: database setup

---
+++ src/database.py
@@ -0,0 +1,24 @@
+import os
+
+from sqlmodel import SQLModel, Session, create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_DB = os.getenv("DATABASE_DB")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+
+DATABASE_URL = f"mysql://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost:{DATABASE_PORT}/{DATABASE_DB}"
+
+engine = create_engine(DATABASE_URL)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
