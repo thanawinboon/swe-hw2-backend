40b96c68290bdbde99c8a8029c6e5b1bac2e62cc
mythicalbadger <jussy12.work@gmail.com>
2024-01-14 10:35:37 +0700

    feat: allow cors for localhost

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 import src.database as database
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers import users, leave_requests
 
 tags_metadata = [
@@ -14,9 +15,21 @@ tags_metadata = [
     },
 ]
 
+origins = [
+    "http://localhost:3000",
+]
+
 
 app = FastAPI(openapi_tags=tags_metadata)
 database.init_db()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(users.router)
